61434f1ab3a7881e1641a87b591ea093185525c1
Ugo Varetto <ugovaretto@gmail.com>
2025-06-08 17:37:02 +0800

    Cleanup

--- alloc-node.sh
+++
@@ -1,3 +0,0 @@
-#1 job max for regular queues
-#salloc -p gpu-dev --gres=gpu:8 --account=pawsey0001-gpu 
-salloc -p gpu --reservation=PAWSEY_GPU_COS_TESTING -A pawsey0001-gpu --exclusive
--- launch-client.sh
+++
@@ -1,1 +0,0 @@
-llama-cli -hf unsloth/gemma-3-4b-it-GGUF:Q8_0 --no-mmap -ngl 999 --rpc "nid002058:6666"
